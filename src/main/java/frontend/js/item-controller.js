dada05fb9ada47acb8696e21dddbdaa4e2866112
Pedro Kuntz <pl_dandrea@hotmail.com>
2019-02-08 00:10:16 -0200

    Add Payment Module

---
+++ src/main/java/frontend/js/item-controller.js
@@ -0,0 +1,23 @@
+angular.module('itemController', [])
+.controller('itemController',['$scope', function($scope){
+  $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+}])
+.filter('listToMatrix', function() {
+    function listToMatrix(list, elementsPerSubArray) {
+        var matrix = [], i, k;
+
+        for (i = 0, k = -1; i < list.length; i++) {
+            if (i % elementsPerSubArray === 0) {
+                k++;
+                matrix[k] = [];
+            }
+
+            matrix[k].push(list[i]);
+        }
+
+        return matrix;
+    }
+    return function(list, elementsPerSubArray) {
+        return listToMatrix(list, elementsPerSubArray);
+    };
+});
